b8967865112b9552c99b134cbeb4b076ce6ca69c
ORyan-01 <joaquin.oryan.c@mail.pucv.cl>
2026-09-14 15:07:18 -0300

    Actualizar tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,38 +1,9 @@
 `default_nettype none
 `timescale 1ns / 1ps
-
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
-
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
-
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-
-  // Replace tt_um_example with your module name:
-  tt_um_jmarin_test1 user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
-
+module tb;
+reg clk=0,rst_n=0,ena=1; reg [7:0] ui_in=0; wire [7:0] uo_out,uio_out,uio_oe; tri [7:0] uio_in;
+assign uio_in[5:0]=uio_oe[5:0]?uio_out[5:0]:6'bz;
+assign uio_in[7:6]=8'bz;
+tt_um_jmarin_3lfcc dut(.ui_in(ui_in),.uo_out(uo_out),.uio_in(uio_in),.uio_out(uio_out),.uio_oe(uio_oe),.ena(ena),.clk(clk),.rst_n(rst_n));
+always #18.5185 clk=~clk; initial begin $dumpfile("tb.fst");$dumpvars(0,tb);#100 rst_n=1;#100000 $finish;end
 endmodule
