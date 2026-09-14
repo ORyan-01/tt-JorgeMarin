4c62bd4921076692ca20b0d8a2374a9d7394fe7e
ORyan-01 <joaquin.oryan.c@mail.pucv.cl>
2026-09-14 15:12:34 -0300

    Actualizar project.v

--- src/project.v
+++ src/project.v
@@ -1,37 +1,26 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
-
-module tt_um_jmarin_test1 (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_jmarin_3lfcc (
+    input wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input wire ena,
+    input wire clk,
+    input wire rst_n
 );
-
-  // All output pins must be assigned. If not used, assign to 0.
-  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uo_out  = fib;
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-reg [7:0] a = 0, b = 1, fib = 0;
-
-always @(posedge clk) begin
-    a <= b;
-    b <= a + b;
-    fib <= a;
-end
-
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, rst_n, 1'b0};
-
+    wire [7:0] pwm;
+    wire scl1, scl2, sda1_o, sda2_o, sda1_oe, sda2_oe;
+    wire oled_sclk, oled_sdin, oled_cs, oled_dc;
+    top u_top (
+        .clk_i(clk), .rst_ni(rst_n),
+        .scl_1_o(scl1), .sda_1_i(uio_in[6]), .sda_1_o(sda1_o), .sda_1_oe(sda1_oe),
+        .scl_2_o(scl2), .sda_2_i(uio_in[7]), .sda_2_o(sda2_o), .sda_2_oe(sda2_oe),
+        .pwm_o(pwm), .ioSclk(oled_sclk), .ioSdin(oled_sdin), .ioCs(oled_cs), .ioDc(oled_dc),
+        .btn1(ui_in[0]), .uart_rx_i(ui_in[1])
+    );
+    assign uo_out = pwm;
+    assign uio_out = {sda2_o, sda1_o, oled_dc, oled_cs, oled_sdin, oled_sclk, scl2, scl1};
+    assign uio_oe = {sda2_oe, sda1_oe, 6'b111111};
+    wire _unused = &{ena, 1'b0};
 endmodule
